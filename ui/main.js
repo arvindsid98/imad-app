04656cb94ea074f0d3d25db7ea4ce03938208ec5
arvindsid98 <30623644+arvindsid98@users.noreply.github.com>
2017-08-13 00:01:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,11 @@ var element=document.getElementById("main-text");
 element.innerHTML="New value";
 
 var log=document.getElementById("im");
+var marginleft=0;
+function moveright() {
+    marginleft=marginleft+10;
+    log.style.marginleft=marginleft+'px';
+}
 log.onclick= function(){
-    log.style.marginLeft='100px';
-    
+    var interval=setInterval(moveright,100);
 };
