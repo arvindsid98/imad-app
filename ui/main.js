ac9d2ecea855730cf06876f7ddeeb47706637f92
arvindsid98 <30623644+arvindsid98@users.noreply.github.com>
2017-08-15 13:04:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,3 +18,18 @@ button.onclick=function(){
     request.send(null);
 };
 
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var sumbit=document.getElementById("submit_btn");
+submi.onclick= function(){
+
+    var names=['name1','name2','name3','name4'];
+    var list="";
+    for(var i=0;i<names.length;i++){
+        list+="<li>"+names[i]+"</li>";
+        
+    }
+    var ul=document.getElementById("namelist");
+    ul.innerHTML=list;
+    
+}
