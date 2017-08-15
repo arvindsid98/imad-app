90d4b1fd0a33a533a64215cbff76e0711405476f
arvindsid98 <30623644+arvindsid98@users.noreply.github.com>
2017-08-15 13:26:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,14 +7,9 @@ button.onclick=function(){
     request.onreadystatechange= function(){
         if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200){
-               var names=['name1','name2','name3','name4'];
-                var list="";
-                for(var i=0;i<names.length;i++){
-                list+="<li>"+names[i]+"</li>";
-        
-                }
-                var ul=document.getElementById("namelist");
-                ul.innerHTML=list; 
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML= counter.toString();
             }
         }
         
@@ -25,9 +20,29 @@ button.onclick=function(){
 
 var nameInput=document.getElementById("name");
 var name=nameInput.value;
-var submit=document.getElementById("submit_btn");
-submit.onclick= function(){
-
-  
+var sumbit=document.getElementById("submit_btn");
+submi.onclick= function(){
+    
+    var request= new XMLHttpRequest();
+    
+    request.onreadystatechange= function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var names= request.responseText;
+                names=JSON.parse(names);
+                var list="";
+                for(var i=0;i<names.length;i++){
+                    list+="<li>"+names[i]+"</li>";
+        
+                }   
+                var ul=document.getElementById("namelist");
+                ul.innerHTML=list;
+ 
+            }
+        }
+        
+    };
+    request.open('GET','http://aravind951.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
     
 }
