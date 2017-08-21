35693552d19a5a1ef3913b5536e09283d11cb366
arvindsid98 <30623644+arvindsid98@users.noreply.github.com>
2017-08-21 23:57:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/test-db', function(req,res){
         if(err){
             res.status(500).send(err,toString());
         }
-        res.send(JSON.stringify(result.rows));
+        res.send(JSON.stringify(result.row));
     });
     
 });
