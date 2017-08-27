2c7e3520fcd4e7d04615420d96a497f03e8ee9f7
arvindsid98 <30623644+arvindsid98@users.noreply.github.com>
2017-08-27 19:25:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,50 +1,30 @@
-var button=document.getElementById("counter");
 
-button.onclick=function(){
-    
-    var request= new XMLHttpRequest();
-    
-    request.onreadystatechange= function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-            if(request.status===200){
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML= counter.toString();
-            }
-        }
-        
-    };
-    request.open('GET','http://aravind951.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
-
-
+//submit uname password
 var submit=document.getElementById("submit_btn");
 submit.onclick= function(){
     
     var request= new XMLHttpRequest();
-    
     request.onreadystatechange= function(){
         if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200){
-                var names= request.responseText;
-                names=JSON.parse(names);
-                var list="";
-                for(var i=0;i<names.length;i++){
-                    list+="<li>"+names[i]+"</li>";
-        
-                }   
-                var ul=document.getElementById("namelist");
-                ul.innerHTML=list;
- 
+                console.log("login success!");
+            }
+            else if(request.status===403){
+                console.log("No such user!")
+            }
+            else if(request.status===500){
+                console.log("Database error!");
             }
         }
         
     };
-    var nameInput=document.getElementById("name");
-    var name=nameInput.value;
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    console.log(username);
+    console.log(password);
     
-    request.open('GET','http://aravind951.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
+    request.open('POST','http://aravind951.imad.hasura-app.io/login',true);
+    request.setRequestHeader('contetn-type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
     
 }
